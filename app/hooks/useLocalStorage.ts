99cb705db3d2bd5aaee6d1b7aae1fd3b1b007ea1
Natanael2005 <165595367+Natanael2005@users.noreply.github.com>
2025-02-04 07:35:59 -0500

    Cambios

--- app/hooks/useLocalStorage.ts
+++ app/hooks/useLocalStorage.ts
@@ -3,10 +3,8 @@
 import { useState, useEffect } from "react"
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
-  // Inicializamos el estado con el valor inicial
   const [storedValue, setStoredValue] = useState<T>(initialValue)
 
-  // Una vez que el componente se monta, intentamos obtener el valor del localStorage
   useEffect(() => {
     try {
       const item = window.localStorage.getItem(key)
@@ -18,7 +16,6 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
     }
   }, [key])
 
-  // Cuando el valor cambia, lo guardamos en localStorage
   useEffect(() => {
     try {
       window.localStorage.setItem(key, JSON.stringify(storedValue))
